9dd32d949c9f6ac168684bec637529ee110a297c
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-21 11:33:47 -0800

    Listen for 'submit' events on the journal entry form and:  Put the form's input values into a new object.  Add the nextEntryId to the object.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 /* exported data */
 var $urlInputBox = document.querySelector('#photoUrl');
 var $photoUrl = document.querySelector('.img-box');
+var $entryForm = document.querySelector('#entry-form');
 
 function handleInput(event) {
   if (event.target.matches('#photoUrl') === true) {
@@ -10,4 +11,18 @@ function handleInput(event) {
   }
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  var entry = {
+    title: $entryForm.elements.title.value,
+    photoUrl: $entryForm.elements.photoUrl.value,
+    notes: $entryForm.elements.notes.value
+  };
+  entry.nextEntryId = data.nextEntryId;
+  data.entries.push(entry);
+  // console.log(entry);
+  // console.log(data.entries);
+}
+
 $urlInputBox.addEventListener('input', handleInput);
+$entryForm.addEventListener('submit', handleSubmit);
