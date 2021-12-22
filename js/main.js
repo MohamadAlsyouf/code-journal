7a8576b3f0a92a3d4bd37ed51774ca4aa5eeb07d
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-22 12:56:25 -0800

    Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired.

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ function handleInput(event) {
   $photoUrl.setAttribute('src', $urlInputBox.value);
 }
 
+var entry = {
+  title: $entryForm.elements.title.value,
+  photoUrl: $entryForm.elements.photoUrl.value,
+  notes: $entryForm.elements.notes.value
+};
 function handleSubmit(event) {
   event.preventDefault();
-  var entry = {
-    title: $entryForm.elements.title.value,
-    photoUrl: $entryForm.elements.photoUrl.value,
-    notes: $entryForm.elements.notes.value
-  };
   entry.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(entry);
@@ -22,5 +22,48 @@ function handleSubmit(event) {
   $entryForm.reset();
 }
 
+function renderEntry(entry) {
+  var entriesLi = document.createElement('li');
+  entriesLi.setAttribute('class', 'entries-li');
+
+  var entriesRowDiv = document.createElement('div');
+  entriesRowDiv.setAttribute('class', 'row');
+  entriesLi.appendChild(entriesRowDiv);
+
+  var imageColumnHalf = document.createElement('div');
+  imageColumnHalf.setAttribute('class', 'column-half');
+  entriesRowDiv.appendChild(imageColumnHalf);
+
+  var entriesImage = document.createElement('img');
+  entriesImage.setAttribute('class', 'column-full img-box');
+  entriesImage.setAttribute('src', entry.photoUrl);
+  imageColumnHalf.appendChild(entriesImage);
+
+  var entriesColumnHalf = document.createElement('div');
+  entriesColumnHalf.setAttribute('class', 'column-half');
+  entriesRowDiv.appendChild(entriesColumnHalf);
+
+  var entriesName = document.createElement('h2');
+  entriesName.setAttribute('class', 'entries-name');
+  entriesName.textContent = entry.title;
+  entriesColumnHalf.appendChild(entriesName);
+
+  var entriesNotes = document.createElement('p');
+  entriesNotes.setAttribute('class', 'entries-notes');
+  entriesNotes.textContent = entry.notes;
+  entriesColumnHalf.appendChild(entriesNotes);
+
+  return entriesLi;
+}
+
+var $ul = document.querySelector('ul');
+function appendDom(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var returnObject = renderEntry(data.entries[i]);
+    $ul.appendChild(returnObject);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', appendDom);
 $urlInputBox.addEventListener('input', handleInput);
 $entryForm.addEventListener('submit', handleSubmit);
