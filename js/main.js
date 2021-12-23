00b38d7094d2b5a01be9781dbdaf7f46d9bfee88
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-23 12:54:50 -0800

    Ensure that submitting a new journal entry also creates a new DOM tree for it and adds it to the page.

--- js/main.js
+++ js/main.js
@@ -3,23 +3,29 @@
 var $urlInputBox = document.querySelector('#photoUrl');
 var $photoUrl = document.querySelector('.img-box');
 var $entryForm = document.querySelector('#entry-form');
+var $newButton = document.querySelector('#new-button');
+var $entryFormView = document.querySelector('#entry-form');
+var $entriesView = document.querySelector('#entries');
+var $entriesLink = document.querySelector('.nav-link');
 
 function handleInput(event) {
   $photoUrl.setAttribute('src', $urlInputBox.value);
 }
 
-var entry = {
-  title: $entryForm.elements.title.value,
-  photoUrl: $entryForm.elements.photoUrl.value,
-  notes: $entryForm.elements.notes.value
-};
 function handleSubmit(event) {
+  var entry = {
+    title: $entryForm.elements.title.value,
+    photoUrl: $entryForm.elements.photoUrl.value,
+    notes: $entryForm.elements.notes.value
+  };
   event.preventDefault();
   entry.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(entry);
   $photoUrl.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
+  var newEntry = renderEntry(entry);
+  $ul.prepend(newEntry);
 }
 
 function renderEntry(entry) {
@@ -57,12 +63,36 @@ function renderEntry(entry) {
 }
 
 var $ul = document.querySelector('ul');
-function appendDom(event) {
+
+function appendDom(entry) {
+  if (data.entries.length === 0) {
+    var noEntries = document.createElement('div');
+    noEntries.setAttribute('class', 'column-full center-text no-entries');
+    noEntries.textContent = 'No entries have been recorded.';
+    $ul.appendChild(noEntries);
+  }
   for (var i = 0; i < data.entries.length; i++) {
     var returnObject = renderEntry(data.entries[i]);
     $ul.appendChild(returnObject);
   }
 }
+
+// view swapping functions
+function handleEntriesNavLink(event) {
+  if (event.target === $entriesLink) {
+    $entriesView.className = 'view';
+    $entryFormView.className = 'hidden';
+  }
+}
+$entriesLink.addEventListener('click', handleEntriesNavLink);
+
+function handleNewClick(event) {
+  if (event.target === $newButton) {
+    $entryFormView.className = 'view';
+    $entriesView.className = 'hidden';
+  }
+}
+$newButton.addEventListener('click', handleNewClick);
 
 document.addEventListener('DOMContentLoaded', appendDom);
 $urlInputBox.addEventListener('input', handleInput);
